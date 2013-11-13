007e89615849e2e3288eeda4751ada1bf01cce0d
Johannes Alneberg <alneberg@kth.se>
2013-11-13 23:21:30 +0100

    Basic concoctr run script

--- scripts/test_concoctr.py
+++ scripts/test_concoctr.py
@@ -3,6 +3,7 @@ import os
 
 file_dirname_path = os.path.dirname(__file__)
 test_data_path = os.path.join(file_dirname_path,"..","tests","test_data")
+result_path = os.path.join(file_dirname_path, "..", "tmp_out_test/")
 composition = os.path.join(test_data_path,"composition.fa")
 coverage = os.path.join(test_data_path, "coverage")
 
@@ -10,8 +11,10 @@ con_p = ConcoctParams(composition,
                       coverage,
                       kmer_length = 4,
                       total_percentage_pca= 80,
-                      limit_kmer_count = 100,
-                      covariance_type = 'full')
+                      length_threshold = 100,
+                      covariance_type = 'full',
+                      basename = result_path)
 
 cr = ConcoctR()
 cr.run_concoct(con_p)
+
