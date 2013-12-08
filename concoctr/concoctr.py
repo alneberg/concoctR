a90446b2c52d73b8e97b0d7545237b3a6d55e2cc
Johannes Alneberg <alneberg@kth.se>
2013-12-08 16:21:47 +0100

    Trying out running with sbatch instead of drmaa

--- concoctr/concoctr.py
+++ concoctr/concoctr.py
@@ -43,7 +43,7 @@ class ConcoctR(object):
     def __init__(self):
         self.log = Logger('ConcoctR')
 
-    def run_concoct(self, concoct_params, drmaa_s=None, drmaa_jt=None):
+    def run_concoct(self, concoct_params, drmaa_s=None, drmaa_jt=None, sbatch_script=None):
         """ drmaa_s is a drama session
         drmaa_jt is a drmaa job template """
         if drmaa_s:
@@ -53,8 +53,21 @@ class ConcoctR(object):
                     drmaa_jt.remoteCommand, job_id))
             return job_id
         else:
-            cla = ['concoct'] + concoct_params.args()
-            subprocess.Popen(cla)
-            self.log.info("Command line call with command: {0}".format(cla))
+            if sbatch_script:
+                subprocess.Popen(['sbatch', sbatch_script])
+            else:
+                cla = ['concoct'] + concoct_params.args()
+                subprocess.Popen(cla)
+                self.log.info("Command line call with command: {0}".format(cla))
 
             return None
+
+   def generate_sbatch_script(self, concoct_params, sbatch_params, file_name):
+       """ Generate a shell script that can be submitted with sbatch. """
+       with open(file_name, 'w+') as f:
+           f.write("#!/bin/bash" + '\n')
+           for sp in sbatch_param:
+               f.write("#SBATCH " + sp + '\n')
+           command = " ".join(['concoct'] + concoct_params.args())
+           f.write(command + '\n')
+       
